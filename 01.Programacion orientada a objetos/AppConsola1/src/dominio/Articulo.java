3d9a22145d64ecb693a97badeba6ff4867a33b9c
Mcanita <albadebiase@gmail.com>
2025-03-25 15:52:44 -0300

    Clases -Encapsulamiento - Método toString()

---
+++ 01.Programacion orientada a objetos/AppConsola1/src/dominio/Articulo.java
@@ -0,0 +1,50 @@
+package dominio;
+
+public class Articulo {
+	
+	//atributos
+	
+	private int id;
+	private String nombre;
+	
+	//constructores
+	
+	public Articulo()
+	{
+		id = 0;
+		nombre = " sin nombre ";
+	}
+	
+	public Articulo (String nombre, int id)
+	{
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	// getters y setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Articulo ID=" + id + ", NOMBRE=" + nombre;
+	}
+	
+	
+	//método toString()
+	
+	
+	
+	
+
+}
