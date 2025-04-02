75cd03c4058ff9373dc67a669df6b88f494880b1
Mcanita <albadebiase@gmail.com>
2025-04-01 22:21:36 -0300

    Interfaces

---
+++ 02.Programacion orientada a objetos/08.interfaces1/AppConsola4/src/ejemplo1/ICantar.java
@@ -0,0 +1,18 @@
+package ejemplo1;
+
+public interface ICantar {
+
+	
+	//Métodos abstractos
+	
+	public void cantar();
+	
+	//Atributos publicos, constantes y estáticos
+	
+	public final static int var = 8;
+	
+	int var2 = 9; // dentro de la interfaz no hace falta aclarar que es constante,estático y publico
+}
+
+
+
