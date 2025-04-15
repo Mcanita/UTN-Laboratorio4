6ba6c5b72436c22416b33a175aff325d877ded8c
Mcanita <albadebiase@gmail.com>
2025-04-15 08:56:43 -0300

    Archivos ( BufferedReader - BufferedWrite )

---
+++ 03.Programacion orientada a objetos/21.archivoc(BufferedReader - BufferedWriter)/AppConsola7/src/ejemplo1/Principal.java
@@ -0,0 +1,39 @@
+package ejemplo1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	
+		
+		Archivo archivo = new Archivo();
+		archivo.setRuta("prueba.txt");
+		
+		if(archivo.existe())
+		{
+			System.out.println("Exciste el archivo");
+		}
+		else
+		{
+			System.out.println("No exciste el archivo, entonces lo creo");
+			archivo.crearArchivo();
+		}
+		
+		//ESCRITURA
+		archivo.escribe_letra_x_letra(" Bienveninos a Lab IV ");
+		
+		//LECTURA
+		System.out.println("\n A CONTINUACIÓN LEE LA INFORMACIÓN DEL ARCHIVO LETRA POR LETRA: ");
+		archivo.lee_letra_x_letra();
+		
+		//ESCRITURA LINEAS
+		archivo.escribe_lineas("Primer Cuatrimestre \n Carrera TSSI ");
+		
+		//LECTURA LINEAS
+		System.out.println("\n A CONTINUACIÓN LEE DE A LINEAS ");
+		archivo.lee_lineas();
+		
+		
+
+	}
+
+}
