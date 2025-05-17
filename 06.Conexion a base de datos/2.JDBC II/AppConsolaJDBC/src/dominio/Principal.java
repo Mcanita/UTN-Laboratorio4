916c689235e394e8766e4cffcb8d6aaf17a94a71
Mcanita <albadebiase@gmail.com>
2025-05-17 02:06:21 -0300

    Se agrega el trabajo de JDBC II

---
+++ 06.Conexion a base de datos/2.JDBC II/AppConsolaJDBC/src/dominio/Principal.java
@@ -0,0 +1,34 @@
+package dominio;
+
+import java.util.ArrayList;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		UsuarioDao usuarioDao = new UsuarioDao();
+		/*
+		Usuario usuario1 = new Usuario();
+		usuario1.setNombre("Josefina");
+		usuario1.setApellido("Josifino");
+		
+		int filas = usuarioDao.agregarUsuario(usuario1);
+		
+		if(filas ==1)
+			System.out.println("Usuario agregado");
+		else
+			System.out.println("Usuario no agregado");
+			*/
+		Usuario usuario2 = usuarioDao.obtenerUsuario(1);
+		System.out.println(usuario2.toString());
+		
+		ArrayList<Usuario> lUsuario = usuarioDao.obtenerTodosLosUsuarios();
+		for (Usuario user: lUsuario)
+		{
+			System.out.println(user.toString());
+		}
+			
+
+	}
+
+}
---
+++ 06.Conexion a base de datos/2.JDBC II/AppConsolaJDBC/src/dominio/UsuarioDao.java
@@ -0,0 +1,94 @@
+package dominio;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class UsuarioDao {
+	
+	private String host = "jdbc:mysql://localhost:3306/";
+	private String user = "root";
+	private String pass = "root";
+	private String dbName = "dbregistro";
+	
+	public UsuarioDao ()
+	{}
+	
+	public int agregarUsuario(Usuario usuario)
+	{
+		String query = "Insert into usuario(nombre, apellido) values ('"+usuario.getNombre()+" ', '"+usuario.getApellido()+"') ";
+		Connection cn = null;
+		int filas = 0;
+		
+		try 
+		{
+			cn = DriverManager.getConnection(host + dbName, user, pass);
+			Statement st = cn.createStatement();
+			filas = st.executeUpdate(query);
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+	    }
+		return filas;
+	}
+	
+	//metodos
+	
+	public Usuario obtenerUsuario(int id)
+	{
+		Usuario x = new Usuario();
+		Connection cn = null;
+		try 
+		{
+			cn = DriverManager.getConnection(host+dbName, user,pass);
+			Statement st = cn.createStatement();
+			String query = "Select * from usuario where id ="+id;
+			ResultSet rs = st.executeQuery(query);
+			rs.next();
+			x.setNombre(rs.getNString("nombre"));
+			x.setApellido(rs.getNString("apellido"));
+			x.setId(rs.getInt("id"));
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		return x;
+	}
+	
+	public ArrayList <Usuario> obtenerTodosLosUsuarios()
+	{
+		ArrayList<Usuario> lUsuarios = new ArrayList<Usuario>();
+		Connection cn = null;
+		try 
+		{
+			cn = DriverManager.getConnection(host+dbName, user,pass);
+			String query = "Select * from usuario";
+			Statement st = cn.createStatement();
+			ResultSet rs = st.executeQuery(query);
+			while (rs.next())
+			{
+				Usuario x = new Usuario();
+				x.setId(rs.getInt("id"));
+				x.setNombre(rs.getNString("nombre"));
+				x.setApellido(rs.getString("apellido"));
+				lUsuarios.add(x);
+			}
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		return lUsuarios;
+		
+		
+		
+	}
+	
+
+}
